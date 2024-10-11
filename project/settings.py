2af178b6b00eb5c769fb5c6ba5c0fb5f21fab367
Tejas Thawkar <ttthawkar17@gmail.com>
2024-10-11 14:59:46 +0530

    change in settings.py

--- project/settings.py
+++ project/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tejas',
+        'USER': 'tejas',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # or your MySQL server IP
+        'PORT': '3306',        # default MySQL port
     }
 }
+
 
 
 # Password validation
